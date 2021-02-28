001bbd0540e23661080f395d8b874b4e4ff64785
redouan el hammouti <red.hammouti7@gmail.com>
2021-02-28 14:23:17 -0800

    fotmat controller

--- src/main/java/com/redouan/tutorials/controller/Controller.java
+++ src/main/java/com/redouan/tutorials/controller/Controller.java
@@ -1,6 +1,5 @@
 package com.redouan.tutorials.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	
+	private String response = "hello!";
 
 	@GetMapping
 	public String getTest() {
-		return "hola!";
+		return response;
 	}
 }
