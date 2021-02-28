fd12dc808e8af403dffcc518c5a90216f4c47cfb
redouan el hammouti <red.hammouti7@gmail.com>
2021-02-28 14:00:01 -0800

    maven profiles example

---
+++ src/main/java/com/redouan/tutorials/MavenMultipleProfileApplication.java
@@ -0,0 +1,13 @@
+package com.redouan.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenMultipleProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenMultipleProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redouan/tutorials/MavenMultipleProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redouan.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenMultipleProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
